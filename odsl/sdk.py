8746b78cbbecd45f86a4a39775189e90d3a71ab9
Colin Hartley <colin.hartley@opendatadsl.com>
2024-07-16 07:33:54 +0100

    v1.0.6 - Improve use of query parameters

--- odsl/sdk.py
+++ odsl/sdk.py
@@ -2,7 +2,7 @@ import requests
 from msal import PublicClientApplication
 import json
 from odsl import cache
-from urllib.parse import quote
+from urllib.parse import quote, urlencode
 
 
 class ODSL:
@@ -19,38 +19,34 @@ class ODSL:
         if stage == 'prod':
             self.url = 'https://odsl.azurewebsites.net/api/'       
         
-    def get(self, service, source, id):
+    def get(self, service, source, id, params=None):
         if self.token == None:
             print("Not logged in: call login() first")
             return
         headers = {'Authorization':'Bearer ' + self.token["access_token"]}
         eid = quote(id)
-        r = requests.get(self.url + service + "/v1/" + source + "/" + eid, headers=headers)
+        r = requests.get(self.url + service + "/v1/" + source + "/" + eid, headers=headers, params=params)
         if r.status_code == 200:
-            # x = json.loads(r.text, object_hook=lambda d: SimpleNamespace(**d))
             return r.json()
         return r.text
     
-    def list(self, service, source='private', filter=None):
+    def list(self, service, source='private', params=None):
         if self.token == None:
             print("Not logged in: call login() first")
             return
         headers = {'Authorization':'Bearer ' + self.token["access_token"]}
-        params = None
-        if filter:
-            params = filter
         r = requests.get(self.url + service + "/v1/" + source, headers=headers, params=params)
         if r.status_code == 200:
             return r.json()
         return r.text        
 
-    def update(self, service, source, var, options=None):
+    def update(self, service, source, var, params=None):
         if self.token == None:
             print("Not logged in: call login() first")
             return
         headers = {'Authorization':'Bearer ' + self.token["access_token"]}        
         body = json.JSONEncoder().encode(o=var)
-        r = requests.post(self.url + service, headers=headers, data=body)
+        r = requests.post(self.url + service, headers=headers, data=body, params=params)
         print(r.status_code)
 
     def login(self):
